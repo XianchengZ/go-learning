9a20e4c992db144777f00e3c3dbdc7d333cf4700
Andrew Zang <andrewz020529@gmail.com>
2023-01-03 14:14:35 -0500

    02-sub-return a random greeting

--- 02-create-module/greetings/greetings.go
+++ 02-create-module/greetings/greetings.go
@@ -6,6 +6,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
@@ -17,12 +19,32 @@ func Hello(name string) (string, error) {
 			return "", errors.New("empty name")
 		}
 
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
 		// := operator is a shortcut for declaring and initializing a variable in one line.
 		
 		// alternatively:
 		// var message string
-		// message = fmt.Sprintf("Hi, %v. Welcome!", name)
+		// message = fmt.Sprintf(randomFormat(), name)
     
 		return message, nil
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+			"Hi, %v. Welcome!",
+			"Great to see you, %v!",
+			"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
