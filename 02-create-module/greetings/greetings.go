f77c24a7555da445b5e35cb82a92e736cbf4e9c7
Andrew Zang <andrewz020529@gmail.com>
2023-01-03 13:57:43 -0500

    02-subtask return and handle an error

--- 02-create-module/greetings/greetings.go
+++ 02-create-module/greetings/greetings.go
@@ -3,11 +3,20 @@ package greetings
 // declared greetings package to collect related functions
 // implement a hello function to return the greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
     // Return a greeting that embeds the name in a message.
+		// if no name was given, return an error with a message
+
+		if name == "" {
+			return "", errors.New("empty name")
+		}
+
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
 		// := operator is a shortcut for declaring and initializing a variable in one line.
 		
@@ -15,5 +24,5 @@ func Hello(name string) string {
 		// var message string
 		// message = fmt.Sprintf("Hi, %v. Welcome!", name)
     
-		return message
+		return message, nil
 }
